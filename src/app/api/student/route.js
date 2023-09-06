de24fe3732fc6a2a842ca471fd6a25d24ef199cf
JEXB <jippee.1211@gmail.com>
2023-09-06 10:44:16 +0700

    Lab16

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -1,5 +1,6 @@
 import { DB } from "@/app/libs/DB";
 import {
+  zStudentDeleteBody,
   zStudentGetParam,
   zStudentPostBody,
   zStudentPutBody,
@@ -31,6 +32,10 @@ export const GET = async (request) => {
   }
 
   //filter by student id here
+
+  if (studentId !== null) {
+    filtered = filtered.filter((std) => std.studentId === studentId);
+  }
 
   return NextResponse.json({ ok: true, students: filtered });
 };
@@ -98,18 +103,36 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
-
+  const studentId = request.nextUrl.searchParams.get("studentId");
+  const parseResult = zStudentDeleteBody.safeParse({ studentId });
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
   //check if student id exist
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === studentId
+  );
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Student ID does not exist",
+      },
+      { status: 404 }
+    );
+  }
 
   //perform removing student from DB. You can choose from 2 choices
-  //1. use array filter method
-  // DB.students = DB.students.filter(...);
-
-  //or 2. use splice array method
-  // DB.students.splice(...)
+  DB.students.splice(foundIndex, 1);
 
   return NextResponse.json({
     ok: true,
-    message: `Student Id xxx has been deleted`,
+    message: `Student Id ${studentId} has been deleted`,
   });
 };
